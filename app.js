5d90caaa7aa1ccd82090949c2eaeaccad2f7d295
andreadamson <andre.adamson@khk.ee>
2019-11-25 12:27:34 +0200

    abiellumine

--- app.js
+++ app.js
@@ -5,26 +5,29 @@ function Isik(e, p, skp) {
     this.eesnimi = e;
     this.perenimi = p;
     this.synnikuupaev = new Date(skp);
-
-//väljasta täisnimi
-
-this.taisNimi = function() {
-    return `${this.eesnimi} ${this.perenimi}`;
 }
 
-
-// arvuta vanus
-    this.arvutaVanus = function() {
+Isik.prototype.arvutaVanus = function() {
         const vaheSekundites = Date.now() - this.synnikuupaev.getTime();
         const vanusDate = new Date(vaheSekundites);
         const taisAasta = vanusDate.getUTCFullYear();
         const vanus = taisAasta - 1970;
         return vanus;
     }
+
+Isik.prototype.taisNimi = function() {
+    return `${this.eesnimi} ${this.perenimi}`;
+}
+
+//abiellumine
+
+Isik.prototype.abiellus = function(uusPerenimi){
+
+    this.perenimi = uusPerenimi;
 }
 
 const andre = new Isik("Andre","Adamson" ,"07-11-2000");
 const kadi = new Isik("Kadi","zopp" ,"11-11-1569");
-console.log(andre)
-console.log(andre.arvutaVanus())
-console.log(kadi.arvutaVanus())
+kadi.abiellus("vaher")
+
+console.log(kadi)
