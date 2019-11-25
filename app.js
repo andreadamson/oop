6f75d00da09d1474bd8510c78d606be157ff7631
andreadamson <55825764+andreadamson@users.noreply.github.com>
2019-11-25 13:04:40 +0200

    sündmused - klaviatuuri sündmused

--- app.js
+++ app.js
@@ -7,14 +7,30 @@ const text = document.querySelector('h5');
 // kustutame lisUusYlesanne väärtus
 lisaUusYlesanne.value = '';
 
-// vormi saatmine
-form.addEventListener('submit', syndmus);
+// klaviatuur
+// keydown
+// lisaUusYlesanne.addEventListener('keydown', syndmus);
+// keyup
+// lisaUusYlesanne.addEventListener('keyup', syndmus);
+// keypress
+// lisaUusYlesanne.addEventListener('keypress', syndmus);
+// focus
+// lisaUusYlesanne.addEventListener('focus', syndmus);
+// blur
+// lisaUusYlesanne.addEventListener('blur', syndmus);
+// paste
+// lisaUusYlesanne.addEventListener('paste', syndmus);
+// cut
+// lisaUusYlesanne.addEventListener('cut', syndmus);
+// input
+// lisaUusYlesanne.addEventListener('input', syndmus);
+
 
 function syndmus(e){
   // logi sündmuse tüüp
   console.log(`Sündmuse tüüp: ${e.type}`);
-   // kontrollime vormi kaudu saadetud väärtus
+  // kontrollime vormi kaudu saadetud väärtus
   console.log(lisaUusYlesanne.value);
   // väljastame ülesanne HTML-is
   text.innerText = lisaUusYlesanne.value;
-} 
+}
