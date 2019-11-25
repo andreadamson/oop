b5cdf3251603037459072d033182019e6357f0b6
andreadamson <andre.adamson@khk.ee>
2019-11-25 15:06:44 +0200

    Puhastamine

---
+++ app.js
@@ -0,0 +1,42 @@
+// Raamatu konstruktor
+function Raamat(a, p, i){
+    this.autor = a;
+    this.pealkiri = p;
+    this.isbn = i;
+  }
+  
+  // Kasutaja liides - KL
+  // konstruktor
+  function KL() {
+  
+  }
+  
+  // KL funktsionaal
+  // sisendväljade puhastamine
+  KL.prototype.puhastaSisend = function(){
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+  }
+  
+  // kirjeldame raamatu lisamise sündmust
+  document.getElementById('book-form').addEventListener('submit', lisaRaamat);
+  // raamatu lisamise funktsioon
+  function lisaRaamat(e){
+    // võtame andmed vormist
+    const pealkiri = document.getElementById('title').value;
+    const autor = document.getElementById('author').value;
+    const isbn = document.getElementById('isbn').value;
+    // loome raamat andmete põhjal
+    const raamat = new Raamat(pealkiri, autor, isbn);
+  
+    console.log(raamat);
+  
+    // loome kasutaja liidese objekt temaga opereerimiseks
+    const kl = new KL();
+  
+    // puhastame väljad sisestatud andmetest
+    kl.puhastaSisend();
+  
+    e.preventDefault();
+  } 
