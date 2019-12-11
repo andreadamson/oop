265b272c85c8c53401c430b1983dc25ed064a1d1
andreadamson <andre.adamson@khk.ee>
2019-12-11 15:21:04 +0200

    Ilma andmete lugemine on valmis

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+  
 // weather for given location
 const weather = new Weather('Tartu linn');
-//
+// app UI
 const ui = new UI();
 
-cityWeather = weather.weatherData().then(cityWeather => { ui.print(cityWeather)}).catch(error => console.log(error));
+weather.weatherData().then(cityWeather => {
+  ui.print(cityWeather);
+}).catch(error => console.log(error));
 
 
-
-console.log(cityWeather);
-console.log(ui)
+console.log(ui);
