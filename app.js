bdeac65e6372ef38d234af4dafe947077d293b2a
andreadamson <andre.adamson@khk.ee>
2019-12-11 15:10:51 +0200

    Saadud ilmaandmete näitamine

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+// weather for given location
+const weather = new Weather('Tartu linn');
+//
+const ui = new UI();
 
+cityWeather = weather.weatherData().then(cityWeather => { ui.print(cityWeather)}).catch(error => console.log(error));
+
+
+
+console.log(cityWeather);
+console.log(ui)
